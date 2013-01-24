df092d5ba45705beea24c20ba13ce17f7940f179
Hamid Dadkhah <hamid@dadkhah.ca>
2013-01-23 20:32:37 -0800

    Initial Commit

---
+++ CircleTweets/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CircleTweets
+//
+//  Created by Hamid Dadkhah on 2013-01-23.
+//  Copyright (c) 2013 JemblGroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
