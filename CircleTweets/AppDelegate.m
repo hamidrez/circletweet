e2c0ca79bd415fea6377732dbd0ada0400f0f191
Hamid Dadkhah <hamid@dadkhah.ca>
2013-01-23 21:34:40 -0800

    Added the sliding menu and get it to work on iPhone 5

--- CircleTweets/AppDelegate.m
+++ CircleTweets/AppDelegate.m
@@ -7,14 +7,58 @@
 //
 
 #import "AppDelegate.h"
+#import "SHSidebarController.h"
+#import "BlueVC.h"
+#import "RedVC.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
+    
+    [[UINavigationBar appearance] setTintColor:[UIColor whiteColor]];
+    
+    NSMutableArray *vcs = [NSMutableArray array];
+    
+    //6 views
+    for (int i = 1; i <= 6; i++){
+        
+        //Check if index is pair
+        if (i % 2 == 0){
+            
+            //Creating view
+            BlueVC *blue = [[BlueVC alloc] init];
+            
+            //Navigation Controller is required
+            UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:blue];
+            
+            //Dictionary of the view and title
+            NSDictionary *view = [NSDictionary dictionaryWithObjectsAndKeys:nav, @"vc", [NSString stringWithFormat:@"Blue %i", i], @"title", nil];
+            
+            //And we finally add it to the array
+            [vcs addObject:view];
+        }
+        
+        else {
+            
+            RedVC *red = [[RedVC alloc] init];
+            
+            UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:red];
+            
+            NSDictionary *view = [NSDictionary dictionaryWithObjectsAndKeys:nav, @"vc", [NSString stringWithFormat:@"Red %i", i], @"title", nil];
+            [vcs addObject:view];
+            
+        }
+    }
+    
+    //Create controller and set views
+    SHSidebarController *sidebar = [[SHSidebarController alloc] initWithArrayOfVC:vcs];
+    self.window.rootViewController = sidebar;
+    
     [self.window makeKeyAndVisible];
     return YES;
 }
@@ -27,7 +71,7 @@
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
